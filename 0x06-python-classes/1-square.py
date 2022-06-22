2210de1b2e0ecaef730c02858112245f4f335ed6
andrewakligo <jaakligo@gmail.com>
2022-06-22 23:34:58 +0000

    Update on Ox06

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """Class Square that defines a square object
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
     """
-    def __init__(self, size):
-        """Initialize method that stores the size of the square
 
-        Args:
-            param1 (int): size of the square
-        """
+    def __init__(self, size):
+        """Initializes the data."""
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 class Square:
-    """ A class that defines a square by its size
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with optional size.
+    Public instance method: def area(self).
     """
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
+        """Initializes the data."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """ Method that returns the square are of the object
-        """
+        """Returns the current square area."""
         return self.__size ** 2
